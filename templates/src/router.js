e7a2383dafc6d9c933bfc3b40bd27df1b9383ebe
nsc-wuhan <hwu@eppei.com>
2020-06-10 16:23:28 +0800

    update

--- templates/src/router.js
+++ templates/src/router.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import MyComponent from "./components";
+import MyPage from "./pages";
 const BasicRoute = () => (
   <HashRouter>
     <Switch>
-      <Route exact path="/" component={MyComponent} />
+      <Route exact path="/" component={MyPage} />
     </Switch>
   </HashRouter>
 );
